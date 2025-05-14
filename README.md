3a6dd202764988c057035af4d2b0ffb30fc6cd6a
Ano Van <anovanmaximuz@gmail.com>
2025-05-14 04:52:03 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bedcrypto-pay
+# Bedcrypto Pay Generate Using Crypto
+
+This is the payment page option for plans and subscriptions via onchain-base with your from Crypto Community apps
+
+## Installation
+
+```bash
+copy file into webserver path
+```
+
+## Usage
+
+- payment generate from Android app
+- input invoice number and click pay
+- if invoice exist you will redirected to coinbace ecommerce platform
+- make payment as usual
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change. [Contribute] (https://github.com/anovanmaximuz/bedcrypto-pay)
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ pay.php
@@ -0,0 +1,440 @@
+<?php
+$title = "Traders of Crypto Community";
+$notes = "Traders statistics from Crypto Community, preview active days, followers, signals, winrate, roe of traders";
+$desc = $notes;
+$photo = "https://bedcrypto.com/web/img/features/03.png";
+
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>    
+        <meta charset="utf-8">
+        <meta name="facebook-domain-verification" content="soeemyjd6en5ih01kwe4qdokimgd2w" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php echo $title; ?> </title>        
+        <link rel="shortcut icon" href="https://bedcrypto.com/img/icon_hd_3.png" type="image/x-icon">
+        <link rel="icon" href="https://bedcrypto.com/img/icon_hd_3.png" type="image/x-icon">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="description" content="<?php echo $desc; ?>">
+        
+        <!--sosmed-->
+        <meta property="og:description" content="<?php echo $desc; ?>"/>
+        <meta name="twitter:description" content="<?php echo $desc; ?>"/>       
+        <meta name="keywords" content="crypto, cryptocurrency, trader"> 
+        <meta property="og:type" content="article"/>
+        <meta property="og:title" content="<?php echo $title; ?>"/>
+
+        <meta property="og:url" content="https://bedcrypto.com"/>
+        <meta property="og:site_name" content="<?php echo $title; ?>"/>
+        <meta property="article:tag" content="crypto"/>
+        <meta property="article:tag" content="trader"/>
+        <meta property="article:tag" content="signal"/>
+        <meta property="article:tag" content="analytics"/>
+        <meta property="og:image" content="https://bedcrypto.com/img/banner_real.jpg"/>
+        <meta property="og:image:width" content="560"/>
+        <meta property="og:image:height" content="315"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+
+        <meta name="twitter:title" content="<?php echo $title; ?>"/>
+        <meta name="twitter:image" content="https://bedcrypto.com/img/banner_real.jpg"/>
+        <!--end sosmed-->
+        
+		<!-- JS here -->
+        <script src="web/js/modernizr-3.5.0.min.js"></script>
+        <script src="web/js/jquery-1.12.4.min.js"></script>
+        <script src="web/js/popper.min.js"></script>
+        <script src="web/js/bootstrap.min.js"></script>
+        <script src="web/js/one-page-nav-min.js"></script>
+        <script src="web/js/slick.min.js"></script>
+        <script src="web/js/ajax-form.js"></script>
+        <script src="web/js/wow.min.js"></script>
+        <script src="web/js/jquery.scrollUp.min.js"></script>
+        <script src="web/js/jquery.magnific-popup.min.js"></script>
+        <script src="web/js/jquery.nice-select.min.js"></script>
+        <script src="web/js/jquery.countdown.min.js"></script>
+        <script src="web/js/jquery.mb.YT.js"></script>
+        <script src="web/js/plugins.js"></script>
+        <script src="web/js/main.js"></script>
+
+		<!-- CSS here -->
+        <link rel="stylesheet" href="web/css/bootstrap.min.css">
+        <link rel="stylesheet" href="web/css/animate.min.css">
+        <link rel="stylesheet" href="web/css/magnific-popup.css">
+        <link rel="stylesheet" href="web/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="web/css/themify-icons.css">
+        <link rel="stylesheet" href="web/css/slick.css">
+        <link rel="stylesheet" href="web/css/default.css">
+        <link rel="stylesheet" href="web/css/style.css">
+        <link rel="stylesheet" href="web/css/responsive.css">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3YEZPYMGKZ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3YEZPYMGKZ');
+          
+          
+        </script>
+        <style>
+
+    .invoice {
+      background: #fff;
+      padding: 30px;
+      border: 1px solid #dee2e6;
+      border-radius: 10px;
+      margin-top: 50px;
+    }
+    .invoice-title {
+      margin-bottom: 30px;
+    }
+
+            .powered{
+                border-radius: 32px;
+            }
+            .counter{
+                font-size: 50px !important;
+            }
+            .canvasjs-chart-credit{
+                    background-color: white;
+                    width: 100px;
+                    left: 0px !important;
+                    right: unset !important;
+                    color: white !important;
+                    top: 353px !important;
+            }
+            .hero-area-wrap::before {
+                background-image: none;
+            }
+            .features-area-wrap::before{
+                background-image: none;
+            }
+            </style>
+            <script type="application/ld+json">
+               {
+                  "@context": "https://schema.org",
+                  "@type": "HowTo",
+                  "image": {
+                    "@type": "ImageObject",
+                    "url": "https://bedcrypto.com/web/img/slider/01.png"
+                  },
+                  "name": "How to see complete crypto statistics",
+                  "totalTime": "PT2M",
+                  "supply": [
+                    {
+                      "@type": "HowToSupply",
+                      "name": "A Android mobile phone"
+                    }, {
+                      "@type": "HowToSupply",
+                      "name": "A laptop or computer desktop"
+                    }
+                  ],
+                  "tool": [
+                    {
+                      "@type": "HowToTool",
+                      "name": "Crypto Community App"
+                    }
+                  ],
+                  "step":[
+                    {
+                      "@type": "HowToStep",
+                      "name": "Preparations",
+                      "text": "Turn on your laptop or computer desktop",
+                      "image": "https://i0.wp.com/www.murdockcruz.com/wp-content/uploads/2017/10/Laptop-and-PC.jpg",
+                      "url": "https://bedcrypto.com/stats/crypto?step=one"
+                      
+                    }, {
+                      "@type": "HowToStep",
+                      "name": "Browser",
+                      "text": "Open browser(chrome, safari, ie, firefox,etc) and open url https://bedcrypto.com/stats/crypto",
+                      "image": "https://www.superprof.co.id/blog/wp-content/uploads/2020/09/u-memilih-web-browser.jpeg",
+                      "url": "https://bedcrypto.com/stats/crypto?step=two"
+                    }, {
+                      "@type": "HowToStep",
+                      "name": "Install Crypto Community App",
+                      "text": "install or open the cc application if you already have it, click the qr-code icon on the first page of the application, on the upper right side",
+                      "image": "https://bedcrypto.com/web/img/slider/01.png",
+                      "url": "https://bedcrypto.com/stats/crypto?step=three"
+                    }, {
+                      "@type": "HowToStep",
+                      "name": "Scan the QR-Code",
+                      "text": "Back to the browser that has opened https://bedcrypto.com, scan the qr-code using the crypto community app",
+                      "image": "https://bedcrypto.com/web/img/features/03.png",
+                      "url": "https://bedcrypto.com/stats/crypto?step=four"
+                    }
+                  ]
+                }
+                </script>
+            <script type="application/ld+json">
+                {
+                  "@context": "https://schema.org",
+                  "@type": "WebSite",
+                  "url": "https://bedcrypto.com/",
+                  "potentialAction": {
+                    "@type": "SearchAction",
+                    "target": {
+                      "@type": "EntryPoint",
+                      "urlTemplate": "https://bedcrypto.com/trader?q={search_term_string}"
+                    },
+                    "query-input": "required name=search_term_string"
+                  }
+                }
+            </script>
+            <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": ["MobileApplication","FinanceApplication"],
+              "name": "Crypto Community",
+              "operatingSystem": "ANDROID",
+              "applicationCategory": "Application",
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.8",
+                "ratingCount": "7529"
+              },"offers": {
+                "@type": "Offer",
+                "price": "0",
+                "priceCurrency":"USD"
+              }              
+            }
+            </script>
+            <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",              
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Jakarta, Indonesia"
+              },
+              "email": "bedcrypto.official(at)gmail.com",   
+              "brand":"Crypto Community",
+              "logo":"https://bedcrypto.com/img/icon_hd_3.png",
+              "slogan":"Trusted cryptocurrency Market Info, Signals Events, Statistics, Alert, Community",
+              "founder":{
+                    "@context": "https://schema.org",
+                    "@type": "Person",
+                    "email": "mailto:ano.amoeba@gmail.com",
+                    "image": "https://bedcrypto.com/img/owner_cc.jpg",
+                    "jobTitle": "CEO",
+                    "name": "Ano Van",                    
+                    "url": "https://linkedin.com/in/anovan"
+                  },
+              "name": "Crypto Community (CC)",
+              "url":"https://bedcrypto.com"
+            }
+            </script>
+            
+         <script src="https://omnipotent.net/jquery.sparkline/contrib/jquery/jquery-1.7.2.min.js"/></script>
+        <script src="https://omnipotent.net/jquery.sparkline/2.0/jquery.sparkline.js"/></script>        
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript">
+    /* <![CDATA[ */
+    $(function() {        
+
+    });
+    /* ]]> */
+    </script>
+        <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": "https://bedcrypto.com",
+              "logo": "https://bedcrypto.com/img/icon_hd_3.png"
+            }
+            </script>
+            <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": ["MobileApplication","FinanceApplication"],
+              "name": "Crypto Community",
+              "operatingSystem": "ANDROID",
+              "applicationCategory": "Application",
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.8",
+                "ratingCount": "7529"
+              },"offers": {
+                "@type": "Offer",
+                "price": "0",
+                "priceCurrency":"USD"
+              }
+            }
+            </script>
+
+    <style>
+       .bd-placeholder-img {font-size: 1.125rem;text-anchor: middle;-webkit-user-select: none;-moz-user-select: none;user-select: none;}@media (min-width: 768px) {.bd-placeholder-img-lg {font-size: 3.5rem;}}.b-example-divider {height: 3rem;background-color: rgba(0, 0, 0, .1);border: solid rgba(0, 0, 0, .15);border-width: 1px 0;box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);}.b-example-vr {flex-shrink: 0;width: 1.5rem;height: 100vh;}.bi {vertical-align: -.125em;fill: currentColor;}.nav-scroller {position: relative;z-index: 2;height: 2.75rem;overflow-y: hidden;}.nav-scroller .nav {display: flex;flex-wrap: nowrap;padding-bottom: 1rem;margin-top: -1px;overflow-x: auto;text-align: center;white-space: nowrap;-webkit-overflow-scrolling: touch;}
+       .progress{
+        --bs-progress-bg: rgb(255, 99, 132) !important;
+       }
+       .gambar{
+         width: 45px;
+         height: 45px;
+         margin-right: 8px;
+         border-radius: 100%;
+        border: 2px solid #fff;
+        }
+        a{
+            text-decoration: none
+        }
+    </style>
+    </head>
+    <body>
+        <!-- hero-area-wrap -->
+        <div class="hero-area-wrap">
+            <!-- header -->
+            <header id="sticky-header" class="transparent-menu pt-20-mati pb-20">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="main-menu">
+                                <nav class="navbar navbar-expand-lg">
+                                    <a href="/" class="navbar-brand">
+                                        <img src="img/icon_hd_3.png" alt="logo" width="32px"> <strong style="color:black">Bed</strong><span style="color:red">Crypto</span>
+                                    </a>
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                                        <span class="navbar-icon"></span>
+                                        <span class="navbar-icon"></span>
+                                        <span class="navbar-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse" id="navbarNav">
+                                        <ul class="navbar-nav ml-auto">
+                                            <li class="nav-item actisve"><a class="nav-link" href="https://guide.bedcrypto.com/" target="_blank">Guide</a></li>  
+                                        </ul>
+                                    </div>
+                                    <div class="header-btn d-none d-xl-block">                                        
+                                        <a href="/"><button class="btn">Back</button></a>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- header-end -->
+  
+        </div>
+        <!-- hero-area-wrap-end -->
+        
+        <div  class="features-area-wrap" >
+            <section class="features-area pt-150 pb-115">
+                <div class="container">
+                     <div class="row">
+                    <div class="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 text-center">
+                        <div class="section-title mb-20">
+                            <h1>Pay with Crypto</h1>
+                            <p>This is the payment page for plans and subscriptions via onchain-base with your crypto.</p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="row" id="form-invoice" style="display:none">
+                        <div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-5 invoice">
+      <div class="d-flex justify-content-between align-items-center">
+        <h1 class="invoice-title">Invoice</h1>
+        <div class="text-end">
+            <strong>Date:</strong> <span id="inv-created">May 12, 2025</span><br>
+          <strong>Invoice #:</strong> <span id="inv-name">INV-000123</span>
+        </div>
+      </div>
+
+      <hr>
+
+ 
+
+      <div class="row mb-4">
+        <div class="col-12">
+          <p><strong>Service Description:</strong></p>
+          <p id="pay-for">Website development and 1-year hosting plan for Client Name including responsive design, domain registration, and ongoing support.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <!-- Optional: Payment terms or notes -->
+        </div>
+          <div class="col-sm-6 text-end" style="text-align: right;">
+         <h4><strong id="inv-price">Total: $671.00</strong></h4>
+        </div>
+      </div>
+
+      <div class="row mt-4">
+        <div class="col-12 text-center">
+            <a class="btn btn-primary mt-3 w-100" id="inv-link" href="google.com" type="button" ">Pay Now </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+                    </div>
+        <div class=" row" id="form-query">  
+        <div class="col-md-12">
+            
+          <div class="container mt-5">
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <label for="exampleInput" class="form-label">Enter invoice code:</label>
+            <input type="text" class="form-control" id="invoice" placeholder="Type here...">
+            <div id="noDataView" class="mt-3" style="display: none;">
+        <div class="alert alert-danger" id="message">No Data found for this invoice.</div>
+      </div>
+            <button class="btn btn-primary mt-3 w-100" type="button" onclick="sendPayment()">Generate Payment</button>
+          </div>
+        </div>
+      </div>
+        </div>
+        
+      </div>
+                </div>
+            </section>
+        </div>
+        <!-- features-area-wrap -->
+        
+      
+    <script>
+  function sendPayment() {
+    const formInvoice = document.getElementById('form-invoice');    
+    const formQuery = document.getElementById('form-query');  
+    const invoiceValue = document.getElementById('invoice').value;
+    const messageDiv = document.getElementById('message');
+    const noDataView = document.getElementById('noDataView');
+
+    fetch('https://bedcrypto.com/pay/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ invoice: invoiceValue })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.code != 200) {
+            formQuery.style.display = 'block';
+            noDataView.style.display = 'block'; 
+            formInvoice.style.display = 'none';
+            messageDiv.textContent = "Invoice "+invoiceValue+" not found";
+        } else {
+            formInvoice.style.display = 'block';
+            formQuery.style.display = 'none';
+            document.getElementById('inv-name').innerText = invoiceValue
+            document.getElementById('inv-created').innerText = data.data.created 
+            document.getElementById('pay-for').innerText = data.data.pay_for 
+            document.getElementById('inv-price').innerText = "Total: $"+data.data.price+".00"
+            document.getElementById('inv-link').href = data.data.callback  
+                
+            noDataView.style.display = 'none'; 
+        }
+    })
+    .catch(error => {
+      document.getElementById('response').innerText = 'Error: ' + error;
+    });
+  }
+</script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+
+</html>
+
